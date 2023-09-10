ad48aed1dce08a7761769664493894f08c7b8c9b
Cedric Angulo <cdrcangulo@gmail.com>
2023-09-10 20:07:19 +0800

    Add files via upload

---
+++ JS/sched.js
@@ -0,0 +1,77 @@
+// Function to calculate time until a specific day and time
+function calculateTimeUntil(targetDay, targetHour, targetMinute) {
+    const currentDate = new Date();
+    let daysUntilTarget = (targetDay - currentDate.getDay() + 7) % 7; // Calculate days until target day
+    if (daysUntilTarget === 0 && currentDate.getHours() > targetHour) {
+        daysUntilTarget = 7; // If the target day has already passed this week, set it to next week
+    }
+    const targetDate = new Date(currentDate);
+    targetDate.setDate(currentDate.getDate() + daysUntilTarget);
+    targetDate.setHours(targetHour, targetMinute, 0, 0); // Set target time
+
+    return targetDate - currentDate;
+}
+
+function updateSchedules() {
+    // Customize your schedule here (day, hour, minute)
+    const schedule = [
+        { day: 1, hour: 9, minute: 0 },   // Monday 9:00 AM
+        { day: 1, hour: 14, minute: 30 }, // Monday 2:30 PM
+        { day: 2, hour: 9, minute: 0 },   // Tuesday 9:00 AM
+        { day: 2, hour: 14, minute: 30 }, // Tuesday 2:30 PM
+        { day: 2, hour: 17, minute: 0 },  // Tuesday 5:00 PM
+        { day: 3, hour: 10, minute: 0 },  // Wednesday 10:00 AM
+        { day: 3, hour: 14, minute: 30 }, // Wednesday 2:30 PM
+        { day: 3, hour: 17, minute: 0 },  // Wednesday 5:00 PM
+        { day: 4, hour: 11, minute: 0 },  // Thursday 11:00 AM
+        { day: 4, hour: 16, minute: 30 }, // Thursday 4:30 PM
+        { day: 5, hour: 8, minute: 0 },   // Friday 8:00 AM
+        { day: 5, hour: 13, minute: 30 }, // Friday 1:30 PM
+        { day: 6, hour: 13, minute: 0 }   // Saturday 1:00 PM
+    ];
+
+    for (let i = 0; i < schedule.length; i++) {
+        const { day, hour, minute } = schedule[i];
+        const eventTime = calculateTimeUntil(day, hour, minute);
+
+        if (eventTime >= 0) {
+            const days = Math.floor(eventTime / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((eventTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((eventTime % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((eventTime % (1000 * 60)) / 1000);
+
+            let daysText = days === 1 ? "day" : "days"; // Singular or plural "day"
+            let hoursText = hours === 1 ? "hour" : "hours"; // Singular or plural "hour"
+            let minutesText = minutes === 1 ? "minute" : "minutes"; // Singular or plural "minute"
+            let secondsText = seconds === 1 ? "second" : "seconds"; // Singular or plural "second"
+
+            if (days === 0) {
+                daysText = "day";
+            }
+            if (hours === 0) {
+                hoursText = "hour";
+            }
+            if (minutes === 0) {
+                minutesText = "minute";
+            }
+            if (seconds === 0) {
+                secondsText = "second";
+            }
+
+            const scheduleDiv = document.getElementById(`schedule${i + 1}`);
+            scheduleDiv.innerHTML = `${days} ${daysText} ${hours} ${hoursText} ${minutes} ${minutesText} ${seconds} ${secondsText}`;
+        }
+    }
+}
+
+// Update the schedules every second
+setInterval(updateSchedules, 1000);
+
+// Initial update
+updateSchedules();
+
+// Function to get the day name
+function getDayName(day) {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfWeek[day];
+}
---
+++ JS/script.js
@@ -0,0 +1,145 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navMenuOverlay = document.querySelector(".overlay");
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navMenuOverlay.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navMenuOverlay.classList.remove("active");
+}
+
+const navbar = document.querySelector('.navbar');
+const navbarLinks = document.querySelectorAll('.nav-item .nav-link');
+
+navbarLinks.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const navbarHeight = navbar.offsetHeight;
+        const targetPosition = targetElement.offsetTop - navbarHeight;
+        const duration = 20000;
+        const delay = 100;
+        setTimeout(() => {
+            window.scroll({
+                top: targetPosition,
+                left: 0,
+                behavior: 'smooth',
+                duration: duration
+            });
+        }, delay);
+    });
+});
+
+function randomValues() {
+    anime({
+        targets: '.shape-container .el',
+        translateX: function () {
+            return anime.random(-20, 20) + 'vw';
+        },
+        translateY: function () {
+            return anime.random(-20, 20) + 'vh';
+        },
+        scale: function () {
+            return anime.random(0.3, 1.6);
+        },
+        rotate: function () {
+            return anime.random(-360, 360);
+        },
+        duration: function () {
+            return anime.random(2000, 4000);
+        },
+        borderRadius: [
+            {
+                value: '10%'
+            },
+            {
+                value: anime.random(10, 15) + '%', duration: 50
+            },
+            {
+                value: '50%', duration: 500
+            }
+        ],
+        easing: 'easeOutElastic(1, .9)',
+    });
+}
+
+window.addEventListener('load', function () {
+    randomValues();
+    setInterval(randomValues, 4000);
+});
+
+const subContent1 = document.querySelector(".m-1-content");
+const subContent2 = document.querySelector(".m-2-content");
+const subContent3 = document.querySelector(".t-1-content");
+const subContent4 = document.querySelector(".t-2-content");
+const subContent5 = document.querySelector(".t-3-content");
+const subContent6 = document.querySelector(".w-1-content");
+const subContent7 = document.querySelector(".w-2-content");
+const subContent8 = document.querySelector(".w-3-content");
+const subContent9 = document.querySelector(".th-1-content");
+const subContent10 = document.querySelector(".th-2-content");
+const subContent11 = document.querySelector(".f-1-content");
+const subContent12 = document.querySelector(".f-2-content");
+const subContent13 = document.querySelector(".sat-1-content");
+
+const subContent1Button = document.querySelector(".m-1");
+const subContent2Button = document.querySelector(".m-2");
+const subContent3Button = document.querySelector(".t-1");
+const subContent4Button = document.querySelector(".t-2");
+const subContent5Button = document.querySelector(".t-3");
+const subContent6Button = document.querySelector(".w-1");
+const subContent7Button = document.querySelector(".w-2");
+const subContent8Button = document.querySelector(".w-3");
+const subContent9Button = document.querySelector(".th-1");
+const subContent10Button = document.querySelector(".th-2");
+const subContent11Button = document.querySelector(".f-1");
+const subContent12Button = document.querySelector(".f-2");
+const subContent13Button = document.querySelector(".sat-1");
+
+const subContent1Close = document.querySelector(".close-m-1");
+const subContent2Close = document.querySelector(".close-m-2");
+const subContent3Close = document.querySelector(".close-t-1");
+const subContent4Close = document.querySelector(".close-t-2");
+const subContent5Close = document.querySelector(".close-t-3");
+const subContent6Close = document.querySelector(".close-w-1");
+const subContent7Close = document.querySelector(".close-w-2");
+const subContent8Close = document.querySelector(".close-w-3");
+const subContent9Close = document.querySelector(".close-th-1");
+const subContent10Close = document.querySelector(".close-th-2");
+const subContent11Close = document.querySelector(".close-f-1");
+const subContent12Close = document.querySelector(".close-f-2");
+const subContent13Close = document.querySelector(".close-sat-1");
+
+handleSubcontent(subContent1, subContent1Button, subContent1Close);
+handleSubcontent(subContent2, subContent2Button, subContent2Close);
+handleSubcontent(subContent3, subContent3Button, subContent3Close);
+handleSubcontent(subContent4, subContent4Button, subContent4Close);
+handleSubcontent(subContent5, subContent5Button, subContent5Close);
+handleSubcontent(subContent6, subContent6Button, subContent6Close);
+handleSubcontent(subContent7, subContent7Button, subContent7Close);
+handleSubcontent(subContent8, subContent8Button, subContent8Close);
+handleSubcontent(subContent9, subContent9Button, subContent9Close);
+handleSubcontent(subContent10, subContent10Button, subContent10Close);
+handleSubcontent(subContent11, subContent11Button, subContent11Close);
+handleSubcontent(subContent12, subContent12Button, subContent12Close);
+handleSubcontent(subContent13, subContent13Button, subContent13Close);
+
+function handleSubcontent(subContent, button, closeButton) {
+    button.addEventListener("click", () => {
+        subContent.classList.toggle("active");
+    });
+    closeButton.addEventListener("click", () => {
+        subContent.classList.remove("active");
+    });
+}
