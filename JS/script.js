8c5a11d3cfe0ffe81e89169e76795d2f95f1d273
Cedric Angulo <cdrcangulo@gmail.com>
2023-10-20 20:53:55 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -167,8 +167,8 @@ function updateStatus() {
     for (const subject in announcement) {
         const subjectAnnouncements = announcement[subject];
         subjectAnnouncements.forEach((item) => {
+            const announcementDate = new Date(item.datetime); // Declare it here
             if (item.status === "urgent") {
-                const announcementDate = new Date(item.datetime);
                 const timeDiffMinutes = Math.floor((announcementDate - now) / (1000 * 60)); // Convert to minutes
                 if (timeDiffMinutes <= 0) {
                     item.status = "in-progress";
