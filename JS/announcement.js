242381ab1ef43ebf9a64cbe766676f38c0ce9a52
Cedric Angulo <cdrcangulo@gmail.com>
2023-10-20 14:18:13 +0800

    Update announcement.js
    test new object

--- JS/announcement.js
+++ JS/announcement.js
@@ -1,22 +1,121 @@
-const announcement = {
+//const announcement = {
     
-    cc101: "",
+//    cc101: "",
 
-    komfil: "",
+//    komfil: "",
 
-    sts: "",
+//    sts: "",
 
-    itnet: "",
+//    itnet: "",
 
-    pathfit: "",
+//    pathfit: "",
 
-    purcom: "\n• PERFORMANCE (NOV. 7 TUESDAY) \n• REFLECTION \n - Paper Size: short bond paper \n - Font size: 12 \n - Font style: Times New Roman/ Tahoma \n - 1.5 line spacing",
+//    purcom: "\n• PERFORMANCE (NOV. 7 TUESDAY) \n• REFLECTION \n - Paper Size: short bond paper \n - Font size: 12 \n - Font style: Times New Roman/ Tahoma \n - 1.5 line spacing",
 
-    cc100: "",
+//    cc100: "",
 
-    mmw: "\n• EXAM (NOV. 10 FRIDAY) \n• POEM (PRINTED)",
+//    mmw: "\n• EXAM (NOV. 10 FRIDAY) \n• POEM (PRINTED)",
 
-    nstp: "\n• ASSIGNMENT (OCT. 21)",
-}
+//    nstp: "\n• ASSIGNMENT (OCT. 21)",
+//}
 
 // To add newline, type "\n"
+
+const announcement = {
+    cc101: [
+        { 
+            text: "", 
+            status: "", 
+            datetime: "", 
+            range: null 
+        },
+        { 
+            text: "", 
+            status: "" 
+        }
+    ],
+    komfil: [
+        { 
+            text: "", 
+            status: "", 
+            datetime: "", 
+            range: null 
+        },
+        { 
+            text: "", 
+            status: "" 
+        }
+    ],
+    sts: [
+        { 
+            text: "", 
+            status: "", 
+            datetime: "", 
+            range: null 
+        },
+        { 
+            text: "", 
+            status: "" 
+        }
+    ],
+    itnet: [
+        { 
+            text: "", 
+            status: "" 
+        }
+    ],
+    pathfit: [
+        { 
+            text: "", 
+            status: "", 
+            datetime: "", 
+            range: null 
+        },
+        { 
+            text: "", 
+            status: "" 
+        }
+    ],
+    purcom: [
+        { 
+            text: "\n• PERFORMANCE (NOV. 7 TUESDAY)", 
+            status: "urgent", 
+            datetime: "2023-11-20T13:00:00", 
+            range: 4 
+        },
+        { 
+            text: "\n• REFLECTION \n - Paper Size: short bond paper \n - Font size: 12 \n - Font style: Times New Roman/ Tahoma \n - 1.5 line spacing", 
+            status: "basic"
+        }
+    ],
+    cc100: [
+        { 
+            text: "", 
+            status: "" 
+        }
+    ],
+    mmw: [
+        { 
+            text: "\nEXAM (NOV. 10 FRIDAY)", 
+            status: "urgent", 
+            datetime: "2023-11-10T13:00:00", 
+            range: 1 
+        },
+        { 
+            text: "\n[Test Reminder]", 
+            status: "urgent", 
+            datetime: "2023-10-20T14:25:00", 
+            range: 1 
+        },
+        { 
+            text: "Poem (Printed)", 
+            status: "basic" 
+        }
+    ],
+    nstp: [
+        { 
+            text: "Assignment (OCT. 21)", 
+            status: "basic" 
+        }
+    ]
+};
