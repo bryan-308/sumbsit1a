cf464c6349473d16822fe0de7f69f373753f604e
Cedric Angulo <cdrcangulo@gmail.com>
2023-10-21 23:15:35 +0800

    Update announcement.js

--- JS/announcement.js
+++ JS/announcement.js
@@ -7,7 +7,7 @@ const announcement = {
     purcom: "\n• PERFORMANCE (NOV. 7 TUESDAY) \n• REFLECTION \n - Paper Size: short bond paper \n - Font size: 12 \n - Font style: Times New Roman/ Tahoma \n - 1.5 line spacing",
     cc100: "",
     mmw: "\n• EXAM (NOV. 10 FRIDAY) \n• POEM (PRINTED)",
-    nstp: "\n• ASSIGNMENT (OCT. 21)",
+    nstp: "\n• PRINTED ASSIGNMENT (OCT. 28) \n 'I will give you a seat work for tomorrow’s meeting, an activity to be accomplished at the comfort of your own home. This activity will be submitted on October 28, our next meeting' \n <i> - Mr. Walter Pineda Friday 9:56 AM</i>",
 }
 
 // To add newline, type "\n"
