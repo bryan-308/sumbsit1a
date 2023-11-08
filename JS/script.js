b790b96e34dddcf35fa21c222b7f3f0724e0b01b
Cedric Angulo <cdrcangulo@gmail.com>
2023-11-08 18:06:07 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -354,7 +354,7 @@ function updateDisplayedContent() {
                         ? `<span style="color: var(--neutral-100); font-size: 10px;"><i class="far fa-clock"></i> ${formatReadableDate(item.datetime)}</span>`
                         : "";
 
-                    content += `<p><span id="status-circle" class="${getStatusClass(item.status)}" style="background-color: ${getStatusColor(item.status)};"></span> ${item.text} ${deadlineText}</p>`;
+                    content += `<div><span id="status-circle" class="${getStatusClass(item.status)}" style="background-color: ${getStatusColor(item.status)};"></span> ${item.text} ${deadlineText}</div>`;
                 }
             });
             element.innerHTML = content;
